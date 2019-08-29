aa6bfcdc38ebaaf7b0a6323813a7d7e38b8e36be
Franco <fran.deluca@hotmail.com>
2019-08-29 14:35:53 -0300

    commit

--- public/js/master.js
+++ public/js/master.js
@@ -76,5 +76,27 @@ window.onload = function(){
               return true;
              }
             }
+          /*  let boton = document.querySelectorAll('.cerrarsesion');
+            boton.addEventListener("mouseover", function(){
+              Swal.fire({
+  title: 'Are you sure?',
+  text: "You won't be able to revert this!",
+  type: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, delete it!'
+}).then((result) => {
+  if (result.value) {
+    Swal.fire(
+      'Deleted!',
+      'Your file has been deleted.',
+      'success'
+    )
+  }
+})
+}) */
+
+
 
 }
